5fa79c2d57160185b1b164ee16736895ea4fc3c2
jdegand <70610011+jdegand@users.noreply.github.com>
2022-07-26 14:56:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Odin Project - Advanced HTML and CSS - Personal Portfolio
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+
+## Overview
+
+Solution for a challenge from the [Odin Project](https://web.archive.org/web/20220525212002/https://www.theodinproject.com/lessons/node-path-advanced-html-and-css-personal-portfolio).  
+
+### Screenshot 
+
+![](odin-project-personal-portfolio-mobile.png)
+
+![](odin-project-personal-portfolio-mobile-2.png)
+
+![](odin-project-personal-portfolio-mobile-3.png)
+
+![](odin-project-personal-portfolio-tablet.png)
+
+![](odin-project-personal-portfolio-loading.png "get more repos from github api")
+
+![](odin-project-personal-portfolio-tablet-2.png)
+
+![](odin-project-personal-portfolio-tablet-3.png)
+
+![](odin-project-personal-portfolio-desktop.png)
+
+## My process
+
+### Built with
+
+- HTML
+- CSS
+- JavaScript
+- Github Users Api
+
+## Continued development
+
+- Better contact section
+- Add tags to github repos and then map over the tags in the javascript
+- Font change (the fonts they used don't really work best here)
+- Github Api has weird quirks - some repos are that have been updated - don't reflect that - ie cssbattle repo etc
+- Could use EmailJs to get emails from visitors and add form in the contact section - not hard to implement but it will expose data and your email could get spammed
+- Loading div is small so main body collapses.  Rework by having a div wrap the loading div with a set height.  The loading div could be positioned absolutely in the center.  Change the JS to hide the wrapper div instead of the loading div.  
+- Disabling the next page button depends on a hardcoded value - it will have to be updated when I surpass that max number of repos.
+
+## Useful Resources
+
+- [YouTube](https://www.youtube.com/watch?v=FiGynIW0wsQ) - Consuming Github Users Api with React
+- [YouTube](https://www.youtube.com/watch?v=f4D50VnO_Gw) - Vanilla JavaScript display API data
+- [Codepen](https://codepen.io/mandelid/pen/kNBYLJ) - CSS spinner
+- [Thoughtco](https://www.thoughtco.com/wrapping-text-around-image-3466530) - wrap text around an image
+- [Stack Overflow](https://stackoverflow.com/questions/39318043/github-users-api-paging-not-work) - github users api pagination
+- [Stack Overflow](https://stackoverflow.com/questions/29130778/irregular-shape-background-css) - irregular shaped background with css
+- [Blog](https://www.falldowngoboone.com/blog/tips-for-vanilla-javascript-dom-manipulation/) - vanilla js dom manipulation
+- [Geeks for Geeks](https://www.geeksforgeeks.org/how-to-wrap-the-text-around-an-image-using-html-and-css/) - wrap text around an image
---
+++ script.js
@@ -0,0 +1,109 @@
+(function(){
+
+    const loadingDiv = document.getElementById('loading');
+    const repoDiv = document.getElementById('repo-div');
+
+    const COLORS = ['#9e1c1c', '#7c75ca', '#c363b4', '#d88f39', '#5e8f4d']
+
+    const randomInt = (min, max) => {
+        return Math.floor(Math.random() * (max-min + 1) + min)
+    }
+
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+    const myWork = document.querySelector('.my-work');
+
+    let page = 1;
+
+    function getRepos(page){
+
+        var first = repoDiv.firstElementChild;
+        while (first) {
+            first.remove();
+            first = repoDiv.firstElementChild;
+        }
+
+        fetch(`https://api.github.com/users/jdegand/repos?page=${page}&per_page=24&sort=updated`)
+        .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response failed')
+        }
+            return response.json()
+        })
+        .then(data => {
+        loadingDiv.style.display = 'none';
+
+        prevButton.disabled = page === 1 ? true : false;
+        nextButton.disabled = page === 10 ? true : false;
+
+        for (let {id, created_at, description, language, name, updated_at, html_url} of data) {
+            
+            const repoCard = document.createElement('div');
+            repoCard.setAttribute('id', id);
+            repoCard.classList.add('repo-card');
+            
+            const repoCardBackground = document.createElement('div');
+            repoCardBackground.style.backgroundColor = COLORS[randomInt(0, COLORS.length - 1)];
+            repoCardBackground.classList.add('repo-card-background');
+
+            const link = document.createElement('a');
+            link.setAttribute('target', '_blank');
+            link.setAttribute('rel', 'noreferrer');
+            link.setAttribute('href', html_url)
+            link.innerText = name;
+
+            const repoName = document.createElement('h3');
+
+            repoName.append(link)
+
+            const cardBody = document.createElement('div');
+
+            const descriptionParagraph = document.createElement('p');
+            descriptionParagraph.innerText = description;
+            descriptionParagraph.classList.add('playfair');
+            descriptionParagraph.classList.add('bold');
+
+            const languageParagraph = document.createElement('p')
+            languageParagraph.innerText = language;
+
+            const flexDiv = document.createElement('div')
+            flexDiv.classList.add('flex-div')
+
+            const span1 = document.createElement('span')
+            span1.innerText = 'created ' + new Date(created_at).toLocaleDateString();
+
+            const span2 = document.createElement('span')
+            span2.innerText = 'updated ' + new Date(updated_at).toLocaleDateString();
+
+            flexDiv.append(span1, span2)
+
+            cardBody.append(descriptionParagraph, languageParagraph, flexDiv)
+
+            repoCardBackground.append(repoName)
+            
+            repoCard.append(repoCardBackground, cardBody)
+            repoDiv.append(repoCard)
+        }
+    })
+        .catch(error => console.log(error))
+    }
+
+    getRepos(1);
+
+    prevButton.addEventListener('click', ()=> {
+        if(page === 1) return;
+        page--;
+        loadingDiv.style.display = 'flex';
+        getRepos(page);
+        myWork.scrollIntoView();
+    })
+
+    nextButton.addEventListener('click', ()=> {
+        if(page === 11) return;
+        page++;
+        loadingDiv.style.display = 'flex';
+        getRepos(page);
+        myWork.scrollIntoView();
+    })
+
+})()
